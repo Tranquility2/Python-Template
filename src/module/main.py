b98cb2f47c99c49590232046060a27795e9c6b92
Roy Moore <roy@moore.co.il>
2024-07-28 16:10:42 +0300

    Improve baseline and tests

--- src/module/main.py
+++ src/module/main.py
@@ -3,8 +3,17 @@
 import platform
 
 
+def print_info() -> None:
+    """Print info."""
+    print("Python version: ", platform.python_version())
+    print("Platform: ", platform.platform())
+
+
+def do_calculation(a: int, b: int) -> int:
+    """Do a calculation."""
+    return a + b
+
+
 def run() -> None:
     """Run the module."""
     print("Working!")
-    print("Python version: ", platform.python_version())
-    print("Platform: ", platform.platform())
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,20 @@
 
 import pytest
 
-from module.main import run
+from module.main import do_calculation, print_info, run
+
+
+def test_print_info(capsys: pytest.CaptureFixture) -> None:
+    """Test the print_info function."""
+    print_info()
+    captured = capsys.readouterr()
+    assert "Python version: " in captured.out
+    assert "Platform: " in captured.out
+
+
+def test_do_calculation() -> None:
+    """Test the do_calculation function."""
+    assert do_calculation(1, 2) == 3
 
 
 def test_main(capsys: pytest.CaptureFixture) -> None:
